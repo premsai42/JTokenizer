7a990820779db03c8ce0383ba2b7891c7baaedd3
prem sai <konathampremsai4@gmail.com>
2025-06-15 11:59:33 +0530

    first commit

---
+++ src/main/java/com/reds/springsec/repo/UserRepo.java
@@ -0,0 +1,11 @@
+package com.reds.springsec.repo;
+
+import com.reds.springsec.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<Users, Integer> {
+
+    Users findByUsername(String username);
+}
